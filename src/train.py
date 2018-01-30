eba31b8cce2669c43c5e8de10d7fe124394e5b5d
Edward Dixon <dixon.edward@gmail.com>
2018-01-30 17:15:38 +0000

    Model is now trainable... but does it converge?

--- src/train.py
+++ src/train.py
@@ -10,12 +10,13 @@ import argparse
 def main():
     # TODO commandline args
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data")
+    parser.add_argument("--train", required=True)
+    parser.add_argument("--test", required=False)
     args = parser.parse_args()
 
-    input_dimensions = (128,128)
+    input_dimensions = (128,128,3)
     imagenet_expert = Xception(include_top=False, input_shape=input_dimensions)
-    for layer in imagenet_expert:
+    for layer in imagenet_expert.layers:
         layer.trainable = False
 
     model = Sequential()
@@ -26,8 +27,10 @@ def main():
     model.compile(loss = "binary_crossentropy", optimizer="adam", metrics=['accuracy'])
 
     datagen = ImageDataGenerator()
-    datagen.flow_from_directory(directory=args.data)
-    model.fit_generator(ImageDataGenerator(datagen.flow_from_directory(args.data)))
+    model.fit_generator(datagen.flow_from_directory(args.train, target_size=input_dimensions[0:2]),
+                        steps_per_epoch=10,
+                        validation_data=datagen.flow_from_directory(directory=args.test, target_size=input_dimensions[0:2]),
+                        validation_steps=10)
 
 if __name__ == "__main__":
     main()
